1c8206575ce10452e73aa9f4ff1bc60ce6993e62
蔡明超 <caimingcao@tuhu.cn>
2019-03-24 18:55:21 +0800

    feat: button component

---
+++ components/style/themes/index.js
@@ -0,0 +1,70 @@
+const brandPrimary = '#108ee9';
+// const brandPrimaryTap = '#1284d6';
+
+export default {
+  primary: brandPrimary,
+  // 透明度
+  opacity_disabled: 0.3,
+
+  // text color
+  color_text_base: '#000000', // 基色
+  color_text_base_inverse: '#ffffff', // 反基色
+
+
+  // 按压后的颜色
+  fill_base: '#ffffff',
+  fill_tap: '#dddddd',
+  fill_disabled: '#dddddd', // disabled
+
+
+  // 主题色
+  brand_primary: '#108ee9',
+
+  // border
+  border_color_base: '#dddddd',
+
+  // 圆角
+  radius_xs: 2,
+  radius_sm: 3,
+  radius_md: 5,
+  radius_lg: 7,
+
+  // 间距
+  // 水平间距
+  h_spacing_sm: 5,
+  h_spacing_md: 8,
+  h_spacing_lg: 15,
+
+  // 垂直间距
+  v_spacing_xs: 3,
+  v_spacing_sm: 6,
+  v_spacing_md: 9,
+  v_spacing_lg: 15,
+  v_spacing_xl: 21,
+
+  // borderRadius
+  border_width_sm: 0.5,
+  border_width_md: 1,
+  border_width_lg: 2,
+
+  // button
+  button_height: 47,
+  botton_font_size: 18,
+
+  button_height_sm: 23,
+  button_font_size_sm: 12,
+
+  primary_button_fill: brandPrimary,
+  primary_button_fill_tap: '#0e80d2',
+
+  ghost_button_color: brandPrimary,
+  ghost_button_fill_tap: `${brandPrimary}99`,
+
+  warning_button_fill: '#e94f4f',
+  warning_button_fill_tap: '#d24747',
+
+  link_button_fill_tap: '#dddddd',
+  link_button_font_size: 16,
+
+
+};
